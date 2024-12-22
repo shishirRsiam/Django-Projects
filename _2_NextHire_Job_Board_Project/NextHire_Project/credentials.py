3449a2b003bbc6a7194e8631618bbdd4695e0407
shishirRsiam <shishir.siam01@gmail.com>
2024-12-23 00:11:14 +0600

    _2_NextHire_Job_Board_Project Inisialixe Done

---
+++ _2_NextHire_Job_Board_Project/NextHire_Project/credentials.py
@@ -0,0 +1,16 @@
+
+
+SECRET_KEY = 'django-insecure-#4#w(ut*ug@7=%h=k3q%p2$v2212giv$--hbt40c2jo3f8+(5t'
+
+# Database link from railwa.app
+# DJ_DATABASE_URL = 'postgresql://postgres:ZHpdVTsNOBzqCAmlTlqzqCJEqBNzdUsJ@autorack.proxy.rlwy.net:33053/railway'
+DJ_DATABASE_URL = 'postgresql://postgres:ZThPWYNOtCAopWcgWWvpyazRZPrJUbmJ@junction.proxy.rlwy.net:40690/railway'
+
+
+# DATABASE
+DATABASE_ENGINE = 'django.db.backends.postgresql'
+DATABASE_NAME = 'postgres'
+DATABASE_USER = 'postgres.uniozqibrtdhcdrsmxgx'
+DATABASE_PASSWORD = 'qgr3YbbVDjbhB!D'
+DATABASE_HOST = 'aws-0-ap-southeast-1.pooler.supabase.com'
+DATABASE_PORT = '6543'
